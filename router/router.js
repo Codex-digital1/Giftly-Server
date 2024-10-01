2eb0cb0889b6d872bdc1fe9e93f6ee0f6e82a563
Sahadat Hosen <ssar96540@gmail.com>
2024-10-01 19:00:03 +0600

    Added a new route and controller for get user

--- router/router.js
+++ router/router.js
@@ -8,10 +8,12 @@ const payment = require("../controller/payment")
 const updateAGift = require("../controller/updateAGift");
 const deleteAGift = require("../controller/deleteAGift");
 const userCreate = require("../controller/userCreate");
+const getUser = require('../controller/getUser')
 
 
 // Routes
-router.post("/users", userCreate)
+router.post("/users", userCreate);
+router.get('/getUser/:email', getUser)
 router.post("/uploadGift", uploadGift);
 router.get("/getAllGift", getAllGift);
 router.get("/:giftId", getAGift) 
