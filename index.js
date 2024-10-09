7b3f1ace6746e4f4891183f3b1ae02cc092597f6
AmirHossain58 <ahantar58@gmail.com>
2024-10-10 00:12:32 +0600

    remove some code

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ const express = require("express")
 const cors = require("cors")
 require("dotenv").config()
 const mongoose = require("mongoose");
-const socketIO = require('socket.io');
 const router = require("./router/router");
 const app = express()
 const http=require('http')
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
-const orderModel = require('./model/orderModelSchema')
 const SocketIo = require("./chatApp/SocketIo");
  const {notification,NotificationClass} = require('./Notification/notification');
 // Middleware
@@ -33,25 +31,6 @@ SocketIo(server);
 notification(server) 
 const notificationClass=new NotificationClass(server)
 notificationClass.sendAll()
-
-// Define the success route
-// app.post('/payment/success/:tranId', async (req, res) => {
-//   const { tranId } = req.params;
-//   console.log('Transaction ID:', tranId);
-//   try {
-//       const order = await orderModel.findOne({ tran_id: tranId }); 
-//       if (!order) {
-//           return res.status(404).json({ message: "Order not found" });
-//       }
-//       order.payment_status = "Success";
-//       await order.save(); 
-//       // Send response based on the success of the order update
-//       res.redirect(`http://localhost:5173/payment/success/${tranId}`);
-//   } catch (error) {
-//       res.status(500).json({ message: "Payment success handling failed", error });
-//   }
-// });
- 
 
 
 app.get("/", async(_req,res)=>{
